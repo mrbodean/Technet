cc7649ae23d88c0b579adcb75e53bd8b1e750044
Jon aka @MrBoDean <jon.warnken@gmail.com>
2017-06-11 19:32:03 -0400

    Replaced where-object alias

--- Powershell/New-USBBootdisk/New-USBBootDisk.ps1
+++ Powershell/New-USBBootdisk/New-USBBootDisk.ps1
@@ -1,5 +1,26 @@
+<#
+.Synopsis
+   Create usb boot media. The script will prompt for the usb drive to repartition and format
+.Description
+   This script will prompt for the drive to repartition and format. After completing the format the 
+   drive will be made bootable by bootsect.exe. The last step will copy the files in $sourcepath to the 
+   usb device.
+.EXAMPLE
+   New-USBBootDisk.ps1
+.NOTES
+    Author
+        Jon Warnken
+        @MrBoDean
+        jon.warnken@mrbodean.net
+    Bootsect.exe 
+        If this is not available on your system you can get it from the Automated Deployment Kit that is approriate for your 
+        enviroment. https://docs.microsoft.com/en-us/windows-hardware/get-started/adk-install
+    Boot Disk format
+        The script is set to format the drive as ntfs. This can be changed by updating diskpart commands in the $command variable
+#>
+$sourcepath = "C:\BootDisk"
 Write-Host "Warning this process will format the USB device that you select!!" -ForegroundColor Red -BackgroundColor White
-$USBDrives = gwmi win32_diskdrive | ?{$_.interfacetype -eq "USB"} | %{gwmi -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  %{gwmi -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | %{$_.deviceid}
+$USBDrives = Get-WmiObject win32_diskdrive | Where-Object{$_.interfacetype -eq "USB"} | ForEach-Object{Get-WmiObject -Query "ASSOCIATORS OF {Win32_DiskDrive.DeviceID=`"$($_.DeviceID.replace('\','\\'))`"} WHERE AssocClass = Win32_DiskDriveToDiskPartition"} |  ForEach-Object{Get-WmiObject -Query "ASSOCIATORS OF {Win32_DiskPartition.DeviceID=`"$($_.DeviceID)`"} WHERE AssocClass = Win32_LogicalDiskToPartition"} | ForEach-Object{$_.deviceid}
 If(!$USBDrives)
 	{
 		Write-Host "No USB drives found! Please insert the drive and try again!"
@@ -12,16 +33,16 @@ Else
 	}
 If($USBDrives.count -gt 1)
 	{
-		$USBDrive = $USBDrive = Read-Host "Enter the drive letter of the usb device that you would like to load the Publix System Config Image on:"
+		$USBDrive = $USBDrive = Read-Host "Enter the drive letter of the usb device that you would like to load the Boot Image on:"
 	}
 else
 	{
 		$USBDrive = $USBDrives
 	}
-$CheckUSB = gwmi -Query "Select DeviceID From Win32_LogicalDisk Where DeviceID = '$USBDrive' and DriveType = 2"
+$CheckUSB = Get-WmiObject -Query "Select DeviceID From Win32_LogicalDisk Where DeviceID = '$USBDrive' and DriveType = 2"
 If(!$CheckUSB)
 	{
-		Write-Host " The Drive letter entered is not a USB device or the Drive was entered incorrectly!"
+		Write-Host " The Drive letter entered is not a removable USB device or the Drive was entered incorrectly!"
 		Foreach($drive in $USBDrives)
 			{
 				Write-Host " To select $drive enter $drive"
@@ -36,7 +57,6 @@ select Volume $USBDrive
 Clean
 create partition primary
 select partition 1
-active
 format fs=ntfs quick
 active
 exit
@@ -52,5 +72,23 @@ Else
 	{
 		Write-Host "The format of $USBDrive is complete"
 	}
-\PublixPe\bootsect.exe /nt60 $USBDrive /force /mbr
-xcopy "c:\PublixPE\ISO\ProdB\x64\*.*" /e $USBDrive
+bootsect.exe /nt60 $USBDrive /force /mbr
+If($LASTEXITCODE -gt 0)
+	{
+		Write-host "Setting the boot sector failed!"
+		exit
+	}
+Else
+	{
+		Write-Host "Setting the boot sector of $USBDrive is complete"
+	}
+xcopy /herky "$sourcepath\*.*" $USBDrive 
+If($LASTEXITCODE -gt 0)
+	{
+		Write-host "Cpoying the boot image to $USBDrive failed!! Check the drive and try again!"
+		exit
+	}
+Else
+	{
+		Write-Host "Copy of the boot image to $USBDrive is complete"
+	}
