9f39dc412a8e157fde7a40e6d934d5cbb2973ab8
Jon aka @MrBoDean <jon.warnken@gmail.com>
2017-07-01 01:05:41 -0400

    added cvs to track updates declined

--- Powershell/UnPublish-UnUsedCMPatches/UnPublish-UnUsedCMPatches.ps1
+++ Powershell/UnPublish-UnUsedCMPatches/UnPublish-UnUsedCMPatches.ps1
@@ -2,10 +2,14 @@
 .Synopsis
     Decline Updates in WSUS that are not used in COnfiguration Manager
 .DESCRIPTION 
-    Script decline updates in WSUS that are not deployed and not required by any clients
+    Script to decline updates in WSUS that are not deployed and not required by any clients
     in Configuration Manager and that are older than 30 days. 
 .EXAMPLE
     Unpublish-UnUsedCMPatches -SiteCode LAB -SCCMServer LabServer -WSUSServer LabWSUS
+        This will report on the patches that would be declined
+.EXAMPLE
+    Unpublish-UnUsedCMPatches -SiteCode LAB -SCCMServer LabServer -WSUSServer LabWSUS -SkipDecline $False
+        This will report on the patches that would be declined
 .NOTES
     Author Jon Warnken
     @MrBoDean
@@ -19,9 +23,10 @@ Param(
                    ValueFromPipelineByPropertyName=$true)]
         [string]$WsusServer,
         # Use SSL for the WSUS connection. Default value is $False
+        [Parameter()]
         [bool]$UseSSL = $False,
         # Port to use for WSUS connection. Default value is 8530
-        [Parameter(Mandatory=$true)]
+        [Parameter()]
         [int]$PortNumber = 8530,
         # Configuration Manager Site Server with SMS Provider role
         [Parameter(Mandatory=$true,
@@ -32,7 +37,7 @@ Param(
                    ValueFromPipelineByPropertyName=$true)]
         [string]$SiteCode,
         # Skip the decline step. Defaults to $True for safety
-        [Parameter(Mandatory=$true)]
+        [Parameter()]
         [bool]$skipdecline = $True
     )
 Function Unpublish-UnUsedCMPatches{
@@ -43,9 +48,10 @@ Function Unpublish-UnUsedCMPatches{
                    ValueFromPipelineByPropertyName=$true)]
         [string]$WsusServer,
         # Use SSL for the WSUS connection. Default value is $False
+        [Parameter()]
         [bool]$UseSSL = $False,
         # Port to use for WSUS connection. Default value is 8530
-        [Parameter(Mandatory=$true)]
+        [Parameter()]
         [int]$PortNumber = 8530,
         # Configuration Manager Site Server with SMS Provider role
         [Parameter(Mandatory=$true,
@@ -56,11 +62,14 @@ Function Unpublish-UnUsedCMPatches{
                    ValueFromPipelineByPropertyName=$true)]
         [string]$SiteCode,
         # Skip the decline step. Defaults to $True for safety
-        [Parameter(Mandatory=$true)]
+        [Parameter()]
         [bool]$skipdecline = $True
     )
-     
-    $cmpatchlist = Get-WmiObject -Namespace "root\SMS\site_$SiteCode" -class SMS_SoftwareUpdate -ComputerName $SCCMServer|Select-Object LocalizedDisplayName, CI_UniqueID, IsDeployed 
+    $outPath = Split-Path $script:MyInvocation.MyCommand.Path
+    $outDeclineList = Join-Path $outPath "DeclinedUpdates.csv"
+    $outDeclineListBackup = Join-Path $outPath "DeclinedUpdatesBackup.csv"
+    "UpdateID, RevisionNumber, Title, KBArticle, SecurityBulletin, LastLevel" | Out-File $outDeclineList
+    $cmpatchlist = Get-WmiObject -Namespace "root\SMS\site_$SiteCode" -class SMS_SoftwareUpdate -ComputerName $SCCMServer|Select-Object LocalizedDisplayName, CI_UniqueID, IsDeployed, NumMissing
     $cmpatchlistcount = $cmpatchlist.Count
     If($cmpatchlistcount -eq 0){ 
         Throw "Error Collecting patches from $SCCMServer"
@@ -89,7 +98,10 @@ Function Unpublish-UnUsedCMPatches{
         Try{
             $patch = $WsusServerAdminProxy.getUpdate([guid]$item.CI_UniqueID)
             if(($patch.IsDeclined -eq $false) -and ( ($patch.CreationDate) -lt (get-date).AddDays(-30))){
-                #"$($patch.Title) is being declined"
+                if(updatesDeclined -eq 0){
+                    Copy-Item -Path $outDeclinedList -Destination $outDeclineListBackup -Force
+                }
+                "$($patch.Id.UpdateId.Guid), $($patch.Id.RevisionNumber), $($patch.Title), $($patch.KnowledgeBaseArticles), $($patch.SecurityBulletins), $($patch.HasSupersededUpdates)" | Out-File $outDeclineList -Append
                 Write-Progress -Activity "Decline Unused Updates" -Status "Declining update #$i/$cmpatchlistcount - $($item.LocalizedDisplayName)" -PercentComplete $percentComplete -CurrentOperation "$($percentComplete)% complete"
                 If(!($skipdecline)){$patch.Decline()}
                 $updatesDeclined++
