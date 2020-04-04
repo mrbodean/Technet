bcd021da0121784b0e36fb9de48f07659baa23f0
MrBoDean <jon.warnken@gmail.com>
2020-04-04 12:57:21 -0400

    Updated Metadata to publish to powershell gallery

--- Powershell/Update-AzDNSRecordMultiSub/Update-AzDNSRecordMultiSub.ps1
+++ Powershell/Update-AzDNSRecordMultiSub/Update-AzDNSRecordMultiSub.ps1
@@ -1,21 +1,18 @@
 <#PSScriptInfo
 .Description
-    Updates a DNS record for a Azure VM if the record does not match the current public ip.
-    This script works across multiple subscriptions and when properly delegated via Azure Lighthouse across tenants.
+    Updates a DNS record for a Azure VM if the record does not match the current public ip.     This script works across multiple subscriptions and when properly delegated via Azure Lighthouse across tenants.
 .VERSION 
     1.0.0
 .GUID 
     cbb81bef-e9d2-47ce-8059-e97943454e89
 .AUTHOR 
     Jonathan Warnken - @MrBodean - http://www.mrbodean.net/
-    
 .COMPANYNAME 
 
 .COPYRIGHT 
     (C) Jonathan Warnken 2020 All rights reserved.
 .TAGS 
     Azure, Runbook, Automation, DNS
-
 .LICENSEURI
     https://github.com/mrbodean/Technet/blob/master/Powershell/Update-AzDNSRecordMultiSub/License
 .PROJECTURI 
@@ -24,7 +21,6 @@
 
 .EXTERNALMODULEDEPENDENCIES 
     Az
-
 .REQUIREDSCRIPTS 
 
 .EXTERNALSCRIPTDEPENDENCIES 
