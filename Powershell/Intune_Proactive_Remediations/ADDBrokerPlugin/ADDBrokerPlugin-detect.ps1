53ff7aa13591bec8ff90616a38fdc45a0cf2f955
Jonathan Warnken <jon.warnken@gmail.com>
2022-12-09 08:09:28 -0800

    Update ADDBrokerPlugin-detect.ps1
    Extending the logic

--- Powershell/Intune_Proactive_Remediations/ADDBrokerPlugin/ADDBrokerPlugin-detect.ps1
+++ Powershell/Intune_Proactive_Remediations/ADDBrokerPlugin/ADDBrokerPlugin-detect.ps1
@@ -1,1 +1,16 @@
-if(get-appxpackage -Name "Microsoft.AAD.BrokerPlugin"){exit 0}else{exit 1}
+$Domain = 'YourDomainName'
+$users = Get-ChildItem -Path c:\users -Exclude 'Administrator','Public'
+$Compliant = $true
+foreach($user in $users){
+    if(get-appxpackage -Name "Microsoft.AAD.BrokerPlugin" -User "$Domain\$($user.Name)"){
+        #No Issue found
+    }else{
+        #Issue found
+        $Compliant = $false
+    }
+}
+if($Compliant){
+    exit 0
+}else{
+    exit 1
+}
