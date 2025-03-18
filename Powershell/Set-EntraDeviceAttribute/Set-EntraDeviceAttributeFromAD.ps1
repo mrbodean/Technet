ef327847c88d8ebca96db4769d726bbcea6ee923
Jonathan Warnken <jon.warnken@gmail.com>
2025-03-18 14:46:33 -0400

    Create Set-EntraDeviceAttributeFromAD.ps1

---
+++ Powershell/Set-EntraDeviceAttribute/Set-EntraDeviceAttributeFromAD.ps1
@@ -0,0 +1,32 @@
+Import-Module ActiveDirectory
+$ADComputers = Get-ADComputer -Filter *
+if(!(Get-InstalledModule Microsoft.Graph -ErrorAction SilentlyContinue)){
+    Install-Module Microsoft.Graph -Scope AllUsers -Repository PSGallery -Force
+}
+#Connect to MSGraph
+Connect-MgGraph -Scopes Directory.ReadWrite.All, Device.Read.All, Directory.AccessAsUser.All -NoWelcome
+Foreach($Comp in $ADComputers){
+    If($Comp.objectGUID){
+        $device = get-mgdevice -Filter "DeviceID eq '$($Comp.objectGUID)'"
+        if($device.ID){
+            Write-Output "Checking $($Device.DisplayName)"
+            $Attrib15 = $device.AdditionalProperties.extensionAttributes.extensionAttribute15
+            If($Comp.DistinguishedName -like '*OU=Domain Controllers,DC=demo,DC=contoso,DC=com'){$CAT = "|Server"}
+            Elseif($Comp.DistinguishedName -like '*OU=Servers,DC=demo,DC=contoso,DC=com'){$CAT = "|Server"}
+            Else{$CAT = "|Workstation"}
+            if(!($Attrib15 -eq "demo.contoso.com$($CAT)")){
+                $Attributes = @{
+                  "extensionAttributes" = @{
+                    "extensionAttribute15" = "demo.contoso.com$($CAT)"
+                    }
+                 }  | ConvertTo-Json
+                 Write-Output "Setting extensionAttribute15 on $($Device.DisplayName)"
+                 Update-MgDevice -DeviceId $device.ID -BodyParameter $Attributes
+            }
+        }
+        #$device|Format-List
+        #Invoke-MgRestMethod -Method PATCH -Body $Attributes -Uri "https://graph.microsoft.com/beta/devices/$($device.ID)"
+        #Update-MgDevice -DeviceId $device.ID -BodyParameter $Attributes
+        Start-Sleep -Milliseconds 500
+    }
+}
